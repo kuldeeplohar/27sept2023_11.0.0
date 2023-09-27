b49309767eac9533bb0358a164b44c2386685978
kuldeeplohar <loharkuldeep169@gmail.com>
2023-09-27 16:19:45 +0530

    c8

--- pages/flight.js
+++ pages/flight.js
@@ -4,7 +4,7 @@ export default function page() {
   return (
     <section className='col-6 h-100 mt-3'>
     <h1>The Flight</h1>
-    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry</p>
    </section>
   )
 }
--- pages/food.js
+++ pages/food.js
@@ -4,7 +4,7 @@ export default function page() {
   return (
     <section className='col-6 h-100 mt-3'>
     <h1>The food</h1>
-     <p>This food is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
+     <p>This food is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum</p>
    </section>
   )
 }
